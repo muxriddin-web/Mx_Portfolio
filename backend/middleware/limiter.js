6de9ed3fe9e5c9f8b5fd5223375595b3d31e0e28
Muxriddin <otkirovmuxriddin01@gmail.com>
2026-07-11 10:09:56 +0500

    resolve merge conflict in contact.js router

--- backend/middleware/limiter.js
+++ backend/middleware/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 minut
-    max: 100, // 10 minut ichida 5 ta so'rov
+    max: 5, // 10 minut ichida 5 ta xabar yuborish mumkin
     standardHeaders: true,
     legacyHeaders: false,
     message: {
